8618b078ae7369b629b44ffaadf1178f794bb7a7
Rodolfo Figueroa <ficr23@hotmail.com>
2017-03-20 17:47:28 -0600

    Agregado codigo java en MainActivity

--- app/src/main/java/com/rodolfo/android/clickcounter/MainActivity.java
+++ app/src/main/java/com/rodolfo/android/clickcounter/MainActivity.java
@@ -2,12 +2,30 @@ package com.rodolfo.android.clickcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void display(int count){
+        TextView tvCounter = (TextView) findViewById(R.id.counter);
+        tvCounter.setText("" + count);
+    }
+
+    public void clickCounter(View view){
+        counter += 1;
+        display(counter);
+    }
+
+    public void reset(View view){
+        counter = 0;
+        display(counter);
     }
 }
